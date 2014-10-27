56219bb3116e3a9e1420e71e0672b76c66ef803a
Tobias Ebnöther <ebi@gorn.ch>
2014-10-27 21:18:59 +0100

    First version of the webpack dev server

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
 var webpackConfig = require('./webpack.config.js');
 
 // The development server (the recommended option for development)
@@ -68,5 +69,27 @@ gulp.task('webpack:build-dev', function(callback) {
       colors: true
     }));
     callback();
+  });
+});
+
+gulp.task('webpack-dev-server', function() {
+  // modify some webpack config options
+  var myConfig = Object.create(webpackConfig);
+  myConfig.devtool = 'eval';
+  myConfig.debug = true;
+
+  // Start a webpack-dev-server
+  new WebpackDevServer(webpack(myConfig), {
+    publicPath: '/' + myConfig.output.publicPath,
+    contentBase: 'http://localhost:3030',
+    stats: {
+      colors: true
+    }
+  }).listen(8080, 'localhost', function(err) {
+    if (err) {
+      throw new gutil.PluginError('webpack-dev-server', err);
+    }
+    gutil.log('[webpack-dev-server]',
+              'http://localhost:8080/webpack-dev-server/index.html');
   });
 });
