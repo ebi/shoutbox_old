1412cad41902e107fa3279f5bedefb0a87c94414
Tobias Ebnöther <ebi@gorn.ch>
2015-02-21 09:43:52 +0100

    Blacklist favicon

--- middleware/mybbSession.js
+++ middleware/mybbSession.js
@@ -1,9 +1,16 @@
+var _ = require('underscore');
+var URL_BLACKLIST = [
+  '/favicon.ico',
+  '/login',
+];
+
 module.exports = function (req, res, next) {
   if (req.session.validated) {
     return next();
   }
 
-  var backUrl = req.originalUrl !== '/login' ? req.originalUrl : '/';
+  var url = req.originalUrl;
+  var backUrl = !_.contains(URL_BLACKLIST, url) ? url : '/';
   req.session.originalUrl = backUrl;
   res.redirect('/login');
 };
